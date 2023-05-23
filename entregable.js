791f38bdf5992bdfd2908f815bc3ba31ab45b6a9
Francisco Caceres <francaceres98@gmail.com>
2023-05-23 15:39:16 -0300

    entrega 2

--- entregable.js
+++ entregable.js
@@ -1,13 +1,17 @@
+const fs = require("fs");
+
 class ProductManager {
-  #products;
+  #path;
 
   constructor() {
-    this.#products = [];
+    this.#path = "data.json";
   }
 
-  addProduct(title, description, price, thumbnail, code, stock) {
+  addProduct = async (title, description, price, thumbnail, code, stock) => {
+    const data = await this.getProducts();
+
     const product = {
-      id: this.#products.length + 1,
+      id: data.length + 1 || 1,
       title: title,
       description: description,
       price: price,
@@ -20,24 +24,99 @@ class ProductManager {
         return "Error: todos los campos deben ser completados";
       }
     }
-    const codeCheck = this.#products.some((e) => e.code === product.code);
-    if (codeCheck) {
-      return "Error: ya existe un producto con ese código";
+
+    if (data) {
+      const codeCheck = data.some((e) => e.code === product.code);
+      if (codeCheck) {
+        return "Error: ya existe un producto con ese código";
+      }
+      await fs.promises.writeFile(
+        this.#path,
+        JSON.stringify([...data, product]),
+        "utf-8"
+      );
+    } else {
+      await fs.promises.writeFile(
+        this.#path,
+        JSON.stringify([product]),
+        "utf-8"
+      );
     }
-    this.#products.push(product);
+
     return "Éxito";
-  }
+  };
 
-  getProducts() {
-    return this.#products;
-  }
+  getProducts = async () => {
+    return await fs.promises
+      .readFile(this.#path, "utf-8")
+      .then((res) => {
+        return JSON.parse(res);
+      })
+      .catch(() => {
+        return false;
+      });
+  };
 
-  getProductById(id) {
-    for (let product of this.#products) {
+  getProductById = async (id) => {
+    const data = await this.getProducts();
+    for (let product of data) {
       if (product.id === id) {
         return product;
       }
     }
     return "Error: no hay ningún producto con ese id";
-  }
+  };
+
+  updateProduct = async (id, newProduct) => {
+    const data = await this.getProducts();
+    for (let product of data) {
+      if (product.id === id) {
+        const index = data.indexOf(product);
+        data[index] = newProduct;
+        await fs.promises.writeFile(this.#path, JSON.stringify(data), "utf-8");
+        return "Éxito";
+      }
+    }
+    return "Error: no hay ningún producto con ese id";
+  };
+
+  deleteProduct = async (id) => {
+    const data = await this.getProducts();
+    for (let product of data) {
+      if (product.id === id) {
+        const index = data.indexOf(product);
+        data.splice(index, 1);
+        await fs.promises.writeFile(this.#path, JSON.stringify(data), "utf-8");
+        return "Éxito";
+      }
+    }
+    return "Error: no hay ningún producto con ese id";
+  };
 }
+
+// const consultas = async () => {
+//   const manager = new ProductManager();
+//   console.log(
+//     await manager.addProduct("Mouse", "Alto mouse", 3500, "link", "MS2", 100)
+//   );
+
+//   console.log(await manager.getProductById(2));
+
+//   const productos = await manager.getProducts();
+//   if (productos) console.log(productos);
+//   else console.log("No hay productos");
+
+//   await manager.updateProduct(1, {
+//     id: 1,
+//     title: "Mouse 2.0",
+//     description: "Bajo mouse",
+//     price: 2000,
+//     thumbnail: "link",
+//     code: "MS2.0",
+//     stock: 100,
+//   });
+
+//   console.log(await manager.deleteProduct(1));
+// };
+
+// consultas();
