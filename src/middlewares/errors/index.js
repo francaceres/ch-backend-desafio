854e47cb2fa05009f8e566dc35cd48f40f9eb4f5
Francisco Caceres <francaceres98@gmail.com>
2023-09-05 21:42:16 -0300

    Minor fix

--- src/middlewares/errors/index.js
+++ src/middlewares/errors/index.js
@@ -3,10 +3,12 @@ import EnumErrors from "../../utils/errors/Enum.errors.js";
 export default (error, req, res, next) => {
   console.log(error.cause);
   switch (error.code) {
+    case EnumErrors.ERROR_ROUTING:
+      res.json({ status: "error", error: error.name });
+      break;
     case EnumErrors.INVALID_TYPES_ERROR:
       res.json({ status: "error", error: error.name });
       break;
-
     default:
       res.json({ status: "error", error: "Unhandled error" });
   }
